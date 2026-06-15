26b4b598eb507b7b79c322bdb15e86b940765883
ahmoah99-source <ah.mo.ah99@gmail.com>
2026-06-15 18:38:00 +0300

    Create AppNavigator.tsx

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import DashboardScreen from '../screens/DashboardScreen';
+
+const Stack = createNativeStackNavigator();
+
+export const AppNavigator = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Dashboard" component={DashboardScreen} />
+    </Stack.Navigator>
+  );
+};
